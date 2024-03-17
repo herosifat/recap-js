758fb5c1c67ab6591eb0d7508daaf64a8ab28618
Your Namesifathero <sifathero466@gmail.com>
2024-03-17 19:33:59 +0600

    template javaScript

---
+++ templacte.js
@@ -0,0 +1,10 @@
+const a =10;
+const b =10;
+const c =10;
+const result = `The sum of ${a} and ${b} and ${c} is ${a+b+c}
+`;
+console.log(result);
+
+const name =`sifat
+hero student of bubt`;
+console.log(name);
