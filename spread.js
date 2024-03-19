d6d4486fac2079cfa32cc5652af1786018d2c689
Your Namesifathero <sifathero466@gmail.com>
2024-03-19 19:37:01 +0600

    spread

---
+++ spread.js
@@ -0,0 +1,9 @@
+// normal array
+const numbers =Math.max(2,3,5,87,5,43,23,5,8,5,4);
+console.log(numbers);
+
+// three dot using
+
+const number = [34,3,4,3,2,22,4,43432,3434,3];
+const num = Math.max(...number);
+console.log(num);
